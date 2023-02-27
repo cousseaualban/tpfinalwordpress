2e37f274443abf9f26e1858c86568afe8ff6f48c
acousseau <acousseau@projetlineaire.com>
2023-02-27 12:01:17 +0100

    TP Final WordPress v11

--- header.php
+++ header.php
@@ -13,15 +13,32 @@
 
 <body>
     <header>
-        <img src="<?php echo get_template_directory_uri() . '/screenshot.png' ?>" alt="Logo du site" class="logo" />
-        <nav>
-            <?php
-            wp_nav_menu(
-                array(
-                    'menu' => 'Menu principal',
-                    'container' => 'nav'
-                )
-            );
-            ?>
-        </nav>
+        <div class="topnav">
+            <img src="<?php echo get_template_directory_uri() . '/screenshot.png' ?>" alt="Logo du site" class="logo" />
+            <div id="myLinks">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'menu' => 'Menu principal',
+                        'container' => 'nav'
+                    )
+                );
+                ?>
+            </div>
+            <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+        <script>
+            /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+            function myFunction() {
+                var x = document.getElementById("myLinks");
+                if (x.style.display === "block") {
+                    x.style.display = "none";
+                } else {
+                    x.style.display = "block";
+                }
+            }
+        </script>
     </header>
