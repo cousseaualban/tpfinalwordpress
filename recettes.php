7a75d3d12cc375c266c9535347843fa1831eb02c
cousseaualban <cousseaualban@gmail.com>
2023-02-28 15:26:51 +0100

    TP Final WordPress v15

--- recettes.php
+++ recettes.php
@@ -10,50 +10,66 @@
 </h1>
 <div class="nosrecettes">
     <?php the_content() ?>
-    <div class="antipastis">
+    <div class="recette">
+    <h2>Moules farcies</h2>
         <?php if (have_rows('recette_1')): ?>
             <?php while (have_rows('recette_1')):
                 the_row(); ?>
-                <?php the_sub_field('photo_1'); ?>
-                <hr>
-                <?php the_sub_field('ingredients_1'); ?>
-                <hr>
-                <?php the_sub_field('preparation_1'); ?>
-                <hr>
-                <?php the_sub_field('conseils_1'); ?>
-                <hr>
+                <?php $photo_1 = get_sub_field('photo_1'); ?>
+                <?php $size = 'full'; ?>
+                <?php if ($photo_1): ?>
+                    <?php echo wp_get_attachment_image($photo_1, $size); ?>
+                    <hr>
+                    <?php the_sub_field('ingredients_1'); ?>
+                    <hr>
+                    <?php the_sub_field('preparation_1'); ?>
+                    <hr>
+                    <?php the_sub_field('conseils_1'); ?>
+                    <hr>
+                <?php endif; ?>
             <?php endwhile; ?>
         <?php endif; ?>
     </div>
 
-    <div class="risotto">
+    <div class="recette">
+        <h2>Risotto aux potimarrons</h2>
         <?php if (have_rows('recette_2')): ?>
             <?php while (have_rows('recette_2')):
                 the_row(); ?>
-                <?php the_sub_field('photo_2'); ?>
-                <hr>
-                <?php the_sub_field('ingredients_2'); ?>
-                <hr>
-                <?php the_sub_field('preparation_2'); ?>
-                <hr>
-                <?php the_sub_field('conseils_2'); ?>
-                <hr>
+                <?php $photo_2 = get_sub_field('photo_2'); ?>
+                <?php $size = 'full'; ?>
+                <?php if ($photo_2): ?>
+                    <?php echo wp_get_attachment_image($photo_2, $size); ?>
+                    <hr>
+                    <?php the_sub_field('ingredients_2'); ?>
+                    <hr>
+                    <?php the_sub_field('preparation_2'); ?>
+                    <hr>
+                    <?php the_sub_field('conseils_2'); ?>
+                    <hr>
+
+                <?php endif; ?>
             <?php endwhile; ?>
         <?php endif; ?>
     </div>
 
-    <div class="dessert">
+    <div class="recette">
+    <h2>Biscuits aux noix</h2>
         <?php if (have_rows('recette_3')): ?>
             <?php while (have_rows('recette_3')):
                 the_row(); ?>
-                <?php the_sub_field('photo_3'); ?>
-                <hr>
-                <?php the_sub_field('ingredients_3'); ?>
-                <hr>
-                <?php the_sub_field('preparations_3'); ?>
-                <hr>
-                <?php the_sub_field('conseils_3'); ?>
-                <hr>
+                <?php $photo_3 = get_sub_field('photo_3'); ?>
+                <?php $size = 'full'; ?>
+                <?php if ($photo_3): ?>
+                    <?php echo wp_get_attachment_image($photo_3, $size); ?>
+                    <hr>
+                    <?php the_sub_field('ingredients_3'); ?>
+                    <hr>
+                    <?php the_sub_field('preparations_3'); ?>
+                    <hr>
+                    <?php the_sub_field('conseils_3'); ?>
+                    <hr>
+                <?php endif; ?>
             <?php endwhile; ?>
         <?php endif; ?>
     </div>
